6d5745d670c318d9cb6393f436ea6b2948b1394b
Nhan Cao <nhancv92@gmail.com>
2020-03-05 00:09:28 +0700

    integrate express + nodemon

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -5,37 +5,69 @@ import Layout from '../../components/Layout'
 import List from '../../components/List'
 import { User } from '../../interfaces'
 import { sampleFetchWrapper } from '../../utils/sample-api'
+import {Component} from "react";
 
 type Props = {
   items: User[]
   pathname: string
 }
 
-const WithInitialProps: NextPage<Props> = ({ items, pathname }) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
-    <h1>Users List</h1>
-    <p>
-      Example fetching data from inside <code>getInitialProps()</code>.
-    </p>
-    <p>You are currently on: {pathname}</p>
-    <List items={items} />
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-)
+export default class extends Component<Props, {}> {
+    // `nextApp.render` sends the given map of values here, under `ctx`
+    // We return a simple map that will be available under this.props
+    // throughout this component
+    static async getInitialProps(ctx) {
+        return {
+            items: ctx.query.items,
+            pathname: ctx.pathname
+        };
+    }
 
-WithInitialProps.getInitialProps = async ({ pathname }) => {
-  // Example for including initial props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const items: User[] = await sampleFetchWrapper(
-    'http://localhost:3000/api/users'
-  )
-
-  return { items, pathname }
+    render() {
+        const {items} = this.props;
+        return <Layout title="Users List | Next.js + TypeScript Example">
+            <h1>Users List</h1>
+            <p>
+                Example fetching data from inside <code>getInitialProps()</code>.
+            </p>
+            <p>You are currently on: {this.props.pathname}</p>
+            <List items={items}/>
+            <p>
+                <Link href="/">
+                    <a>Go home</a>
+                </Link>
+            </p>
+        </Layout>
+    }
 }
 
-export default WithInitialProps
+
+//
+// const WithInitialProps: NextPage<Props> = ({ items, pathname }) => (
+//   <Layout title="Users List | Next.js + TypeScript Example">
+//     <h1>Users List</h1>
+//     <p>
+//       Example fetching data from inside <code>getInitialProps()</code>.
+//     </p>
+//     <p>You are currently on: {pathname}</p>
+//     <List items={items} />
+//     <p>
+//       <Link href="/">
+//         <a>Go home</a>
+//       </Link>
+//     </p>
+//   </Layout>
+// )
+//
+// WithInitialProps.getInitialProps = async ({ pathname }) => {
+//   // Example for including initial props in a Next.js function component page.
+//   // Don't forget to include the respective types for any props passed into
+//   // the component.
+//   const items: User[] = await sampleFetchWrapper(
+//     'http://localhost:3000/api/users'
+//   )
+//
+//   return { items, pathname }
+// }
+
+// export default WithInitialProps
